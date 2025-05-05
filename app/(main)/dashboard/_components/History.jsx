0d4e59a5601303bd91c507e387bbd18571c66c9f
Anshul <anshulbhaskar50@gmail.com>
2025-05-05 22:27:39 +0530

    Simple change

--- app/(main)/dashboard/_components/History.jsx
+++ app/(main)/dashboard/_components/History.jsx
@@ -4,7 +4,7 @@ function History() {
   return (
     <div>
       <h2 className='font-bold text-xl'>Your Previous Lectures</h2>
-      <h2 className="text-400-gray">You don't have any previous lectures</h2>
+      <h2 className="text-450-gray">You don't have any previous lectures</h2>
     </div>
   )
 }
